24eecdf6090d2e8cb30bfcce7640ceaee2eeba84
flacfile <129928541+flacfile@users.noreply.github.com>
2023-06-08 18:50:47 +0000

    Programma jautā, bet neiet caur nakamo jautājumu

--- main.cpp
+++ main.cpp
@@ -11,5 +11,26 @@ struct Question {
 };
 
 int main() {
-  cout << "Hello World!\n";
+    // Izveidojiet jautājumu sarakstu
+    vector<Question> jautajumi = {
+        {
+           "Kāda ir C++ funkcija, lai iegūtu maksimālo vērtību?",
+            {"max()", "maxval()", "maximum()", "fmax()"},
+            {0, 3}
+        },
+        {
+      
+        },
+        // Citi jautājumu, pagaidam neizdomāja
+    };
+
+// Izveidojiet rezultātu mainīgos
+    int pareizasAtbildes = 0;
+    vector<int> nepareizasAtbildes;
+
+  // Iziet cauri katram jautājumam
+    for (int i = 0; i < jautajumi.size(); i++) {
+    cout << "Jautājums " << (i + 1) << ": " << jautajumi[i].jautajums << "\n";
+        cout << "Izvēlieties atbildi (ievadiet atbilžu numuru):" << "\n";
+}
 }
